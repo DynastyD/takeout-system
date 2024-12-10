99f178d589baa3e93a2f161e13199bd6b70752e1
DynastyYZH <dynastyyzh@gmail.com>
2024-12-10 10:47:40 +0100

    Employee paging query business code

--- sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
+++ sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
@@ -18,9 +18,9 @@ import java.time.format.DateTimeFormatter;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 /**
- * 对象映射器:基于jackson将Java对象转为json，或者将json转为Java对象
- * 将JSON解析为Java对象的过程称为 [从JSON反序列化Java对象]
- * 从Java对象生成JSON的过程称为 [序列化Java对象到JSON]
+ * Object mapper: convert Java objects to json based on jackson, or convert json to Java objects
+ * The process of parsing JSON into Java objects is called [Deserializing Java objects from JSON]
+ * The process of generating JSON from Java objects is called [Serializing Java objects to JSON]
  */
 public class JacksonObjectMapper extends ObjectMapper {
 
@@ -31,10 +31,10 @@ public class JacksonObjectMapper extends ObjectMapper {
 
     public JacksonObjectMapper() {
         super();
-        //收到未知属性时不报异常
+        //No exception is reported when unknown attributes are received
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        //反序列化时，属性不存在的兼容处理
+        //Compatibility with non-existent attributes during deserialization
         this.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         SimpleModule simpleModule = new SimpleModule()
@@ -45,7 +45,7 @@ public class JacksonObjectMapper extends ObjectMapper {
                 .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
                 .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
 
-        //注册功能模块 例如，可以添加自定义序列化器和反序列化器
+        //Register a feature module. For example, you can add custom serializers and deserializers.
         this.registerModule(simpleModule);
     }
 }
