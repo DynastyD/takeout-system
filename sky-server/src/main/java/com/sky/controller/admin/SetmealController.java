5695b532bb1e0c0fee2063952f3f608a6fb8f491
DynastyYZH <dynastyyzh@gmail.com>
2024-12-12 16:35:07 +0100

    Cache setmeal code development

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +32,7 @@ public class SetmealController {
 
     @PostMapping
     @ApiOperation("new setmeals")
+    @CacheEvict(cacheNames = "sermealCache", key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         setmealService.saveWithDish(setmealDTO);
         return Result.success();
@@ -45,6 +47,7 @@ public class SetmealController {
 
     @DeleteMapping
     @ApiOperation("Deleting set meal in batches")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         setmealService.deleteBatch(ids);
         return Result.success();
@@ -59,12 +62,14 @@ public class SetmealController {
 
     @PutMapping
     @ApiOperation("Modify set meal")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         setmealService.update(setmealDTO);
         return Result.success();
     }
     @PostMapping("/status/{status}")
     @ApiOperation("Set meal starts and ends")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id) {
         setmealService.startOrStop(status, id);
         return Result.success();
